546dbbc144cbb9a7e40aa950e2640f177c3832bb
Jair Alvaro <imanolax12@gmail.com>
2022-09-12 11:53:24 -0500

    primera version

---
+++ javaScript/FuncionJava.js
@@ -0,0 +1,7 @@
+
+mifuncion(4, 2);
+
+function mifuncion(a, b){
+    console.log("suma: " + (a + b));
+}
+mifuncion(2, 3);
---
+++ javaScript/RDvariable.js
@@ -0,0 +1,12 @@
+
+
+let nombreCompleto = "Juan Perez";
+let nombreCompleto = "Carlos Lara";
+console.log(nombreCompleto);
+console.log(nombreCompleto);
+
+let a1nombreCompleto;
+let _nombreCompleto;
+let $nombreCompleto;
+
+let ruptura = 10;
